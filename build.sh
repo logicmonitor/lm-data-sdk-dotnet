4613636489b275cfa7ed7b9ca9a54162f688dfbc
AakashKhopade <82152762+AakashKhopade@users.noreply.github.com>
2021-12-07 16:16:09 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,8 +3,7 @@ cd /lm-data-sdk-dotnet
 dotnet restore 
 dotnet build
 ls -l
-chown -R 2007:2007 LogicMonitor.DataSDK/bin
-chown -R 2007:2007 LogicMonitor.DataSDK/obj
+chown -R 2007:2007 LogicMonitor.DataSDK
 chown -R 2007:2007 LogicMonitor.DataSDK.Test
 
 ls -l
